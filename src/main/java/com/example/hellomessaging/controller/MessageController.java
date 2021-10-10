850c307a7f22a57e3d310ae3ccef791683a17ec4
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-10-10 05:51:56 +0530

    "Refactored: solve proper for RequestMapping"

--- src/main/java/com/example/hellomessaging/controller/MessageController.java
+++ src/main/java/com/example/hellomessaging/controller/MessageController.java
@@ -1,34 +1,40 @@
 package com.example.hellomessaging.controller;
 
+import com.example.hellomessaging.model.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/hello")
 public class MessageController {
 
-    @GetMapping(value = "/hello")
+    @GetMapping(value = {  "","/", "/hello"})
     public String hello() {
         return "Hello Everyone !!";
+//        return new <ResponseEntity<String>("Hello ", HttpStatus.OK);
     }
 
-    @GetMapping(value = "/hello1")
+    // curl localhost:8080/hello/query?name=Sumesh
+    @RequestMapping(value = "{/query", method = RequestMethod.GET)
     public String hello1(@RequestParam(value = "name") String name) {
         return "Hello " + name;
     }
 
-    @GetMapping(value = "/hello2{name}")
+    // curl localhost:8080/hello/param/Sumesh
+    @GetMapping(value = "/param{name}")
     public String hello2(@PathVariable("name") String name) {
         return "Hello PathVariable " + name;
     }
 
     @PostMapping("/helloPost")
-    public String helloPost(@RequestBody String fname, @RequestBody String lname) {
-        return "Hello " + fname + " " + lname;
+    public String helloPost(@RequestBody User user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + ".";
     }
 
-    @PutMapping("/helloPut")
-    public String helloPut(@PathVariable String firstName,
-                           @RequestParam String lastName) {
+    @PutMapping("/helloPut/{firstName}")
+    public String sayHello(@PathVariable String firstName,
+                           @RequestParam(value = "lastName") String lastName) {
         return "Hello, You are Great " + firstName + " " + lastName + " ..";
     }
 
